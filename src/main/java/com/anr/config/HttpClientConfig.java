b4bccfc0096f3cb41fd8831b836f2e211c105a30
aswarthana <banr20@gmail.com>
2022-12-01 22:35:59 +0400

    spring-boot HTTP client feature implmented.

---
+++ src/main/java/com/anr/config/HttpClientConfig.java
@@ -0,0 +1,24 @@
+package com.anr.config;
+
+import com.anr.httpclient.PeopleClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class HttpClientConfig {
+
+    @Bean
+    public PeopleClient peopleClient(WebClient.Builder builder) {
+        WebClient webClient = builder
+                .baseUrl("http://localhost:9090")
+                .build();
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(webClient)).build();
+        PeopleClient peopleClient = factory.createClient(PeopleClient.class);
+        return peopleClient;
+
+
+    }
+}
