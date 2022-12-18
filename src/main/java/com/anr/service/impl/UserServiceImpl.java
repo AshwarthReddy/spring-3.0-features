bfe8c79c60a0ec579dec385cb9e4cc01da8b8e0e
aswarthana <banr20@gmail.com>
2022-12-18 13:43:15 +0400

    config-properties with records

--- src/main/java/com/anr/service/impl/UserServiceImpl.java
+++ src/main/java/com/anr/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.anr.service.impl;
 
 import com.anr.httpclient.PeopleClient;
 import com.anr.model.People;
+import com.anr.props.MailProperties;
 import com.anr.service.UserService;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 
@@ -18,9 +21,12 @@ public class UserServiceImpl implements UserService {
     private final PeopleClient peopleClient;
 
     private final ObservationRegistry observationRegistry;
+    private final MailProperties mailProperties;
 
+    public static final String GMAIL_FROM = "gmail.from";
     @Override
     public People createUser(People user) {
+        Map<String, String> servers = mailProperties.servers();
 
         People posts = Observation
                 .createNotStarted("json-place-holder.create-users", observationRegistry)
@@ -29,6 +35,9 @@ public class UserServiceImpl implements UserService {
 
 
         People people = peopleClient.savePeople(user);
+
+        String from = servers.get(GMAIL_FROM);
+        System.out.println("from = " + from);
 
 
         return people;
